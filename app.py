8ce9ff3f426df964671f7ec889745e0be3b1bd0b
DivyaMeghana09 <158170572+DivyaMeghana09@users.noreply.github.com>
2026-04-05 23:52:02 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,18 @@ import gradio as gr
 def health():
     return "Fraud API is running ✅"
 
-iface = gr.Interface(fn=health, inputs=[], outputs="text")
+def reset():
+    return "reset successful"
 
-iface.launch(server_name="0.0.0.0", server_port=7860)
+with gr.Blocks() as demo:
+    gr.Markdown("# Fraud API is running ✅")
+    btn = gr.Button("Check")
+    output = gr.Textbox()
+    
+    btn.click(fn=health, inputs=[], outputs=output)
+
+# ✅ THIS LINE IS CRITICAL
+demo.queue()
+
+# ✅ THIS LINE IS CRITICAL
+demo.launch(server_name="0.0.0.0", server_port=7860)
